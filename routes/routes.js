c8e8fd7bf2103b1a6b043af17565191281f2e5a5
seanaus <seanaus1972@googlemail.com>
2026-09-02 17:02:25 +0100

    bug fix

--- routes/routes.js
+++ routes/routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get(`/tasks`, controller.getTasks);
 router.post("/tasks/:dispatchId/hold", controller.holdTask);
-router.post("/tasks/:dispatchId/claim", controller.claimTask);r
+router.post("/tasks/:dispatchId/claim", controller.claimTask);
 router.post("/tasks/:dispatchId/complete", controller.completeTask);
 router.post("/tasks/:dispatchId/revoke", controller.revokeTask);
 router.put("/areas/:areaId/heartbeat", controller.heartbeat);
